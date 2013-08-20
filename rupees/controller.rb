93d8de826794edc450cabd75606df4ef1e9daacf
jcanler <jcanler.ext@orange.com>
2013-08-20 17:38:57 +0200

    [MET-6] Added logger

--- rupees/controller.rb
+++ rupees/controller.rb
@@ -1,21 +1,27 @@
 #controller.rb
 #Metrics Controller
 
+require 'logger'
 require 'singleton'
 
 class Controller 
   include Singleton
   
   def initialize               
+    @logger = Logger.new(STDOUT)
+    @logger.level = Logger::INFO
   end
     
   def run
+    @logger.info("[Controller] Booting Metrics controller aka `BlackBox`...")
+  end
+  
+  def stop
+    @logger.info("[Controller] Exiting Metrics controller.")    
   end
 end
 
 # Boot
-puts("[Controller] Booting Metrics controller aka `BlackBox`...")
-
 Controller.instance.run
+Controller.instance.stop
 
-puts("[Controller] Exiting Metrics controller...")
