a0cd46ac389f04d9d3979516973495eada23af32
djey <j.canler@gmail.com>
2013-09-09 20:56:58 +0200

    [MET-6.5] Verification de ruby

--- scripts/start-metrics.cmd
+++ scripts/start-metrics.cmd
@@ -1,5 +1,15 @@
 @ECHO OFF
+
 REM Starts Metrics Controller for Windows
-REM TODO Should check for prerequisites (ruby)
-ruby ..\rupees\controller.rb -c ..\conf\metrics.win.yml
+
+WHERE /Q ruby
+IF ERRORLEVEL 0 (
+	ECHO ruby script engine found! 
+	ruby --version
+REM TODO check for required version + packages	
+	ruby ..\rupees\controller.rb -c ..\conf\metrics.win.yml
+) ELSE (
+	ECHO ruby script engine not found ! Please check your install.
+) 
+
 PAUSE
--- scripts/start-metrics.sh
+++ scripts/start-metrics.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
+
 #Starts Metrics Controller for Linux
+
 if hash ruby 2>/dev/null; then
+	echo ruby script engine found!
 	ruby --version
-	#ruby ../rupees/controller.rb -h
-	#TODO check for required version + packages
+#TODO check for required version + packages
 	ruby ../rupees/controller.rb -c ../conf/metrics.nux.yml
 else
     echo ruby script engine not found ! Please check your install.
