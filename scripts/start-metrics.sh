4f7f212436a8651781a74bcc0d06522ec1c18eee
jcanler <jcanler.ext@orange.com>
2013-09-09 18:29:51 +0200

    [MET-6.5] (WIP) Verification des pre-requis pour Linux (ruby)

--- scripts/start-metrics.sh
+++ scripts/start-metrics.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 #Starts Metrics Controller for Linux
-#TODO Should check for prerequisites (ruby)
-#ruby ../rupees/controller.rb -h
-ruby ../rupees/controller.rb -c ../conf/metrics.nux.yml
+if hash ruby 2>/dev/null; then
+	ruby --version
+	#ruby ../rupees/controller.rb -h
+	#TODO check for required version + packages
+	ruby ../rupees/controller.rb -c ../conf/metrics.nux.yml
+else
+    echo ruby script engine not found ! Please check your install.
+fi
