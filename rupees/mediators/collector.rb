de1536cf6d5a618352e689291aeaece8080f17da
djey <j.canler@gmail.com>
2013-09-11 20:22:22 +0200

    [MET-6.7] Coquille

--- rupees/mediators/collector.rb
+++ rupees/mediators/collector.rb
@@ -7,7 +7,7 @@ module Collector
   @logger = Logger.new(STDOUT)
   @logger.level = Logger::INFO
     
-  def add(appId, contextId, natureId, value)
+  def self.add(appId, contextId, natureId, value)
     @logger.info("[Collector][add] Data received! appId: #{appId} - contextId: #{contextId} - natureId: #{natureId} - value: #{value}")    
     datas = []
     datas << DataItem.new(appId, contextId, natureId, value)     
