e9a4b439c0bb3239a4b4c8a93ee83db6a5bffdb6
jcanler <jcanler.ext@orange.com>
2013-11-06 10:49:41 +0100

    [MET-6.11] Realisation du test dump

--- test/file/dump.sh
+++ test/file/dump.sh
@@ -1,11 +1,18 @@
 #! /bin/sh
 
 #Test dump feature  WS IN (default port) -> FILE OUT
+echo *Flushing redis...
+/home/avsp/applications/redis-2.6.14/src/redis-cli "FLUSHDB"
+echo
 echo *Starting dump...
 curl -X GET http://localhost:4560/controller/fileOutConnector/start/APP_TEST
 echo
 echo *Go...
-curl -X POST -H "Content-Type: application/json" -d @data/dataCollection.json http://localhost:4567/collector/APP_TEST
+for i in `seq 1 100`
+do
+	curl -X POST -d "" http://localhost:4567/collector/APP_TEST/CTX1/NAT1/$i
+	sleep 0
+done
 echo
 echo *Stopping dump...
 curl -X GET http://localhost:4560/controller/fileOutConnector/stop
